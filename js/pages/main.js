445e83261828939271f66804a4739fbeb66cca00
Ramesh Ghimire <ghimire.9@gmail.com>
2023-11-24 13:36:45 +0100

    okey

---
+++ js/pages/main.js
@@ -0,0 +1,16 @@
+let openBtn = document.querySelector(".bi-list")
+let closeBtn = document.querySelector(".bi-x")
+
+let menu = document.querySelector(".options")
+
+openBtn.addEventListener("click", () => {
+  openBtn.classList.toggle("hide")
+  closeBtn.classList.toggle("hide")
+  menu.classList.toggle("hidden")
+})
+
+closeBtn.addEventListener("click", () => {
+  openBtn.classList.toggle("hide")
+  closeBtn.classList.toggle("hide")
+  menu.classList.toggle("hidden")
+})
